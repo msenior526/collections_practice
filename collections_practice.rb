c9edbc4758cefa1019acad860dd2eebf22c18dec
msenior526 <msenior526@gmail.com>
2020-11-01 16:34:59 -0500

    commit tree fe5576f76d57d2f05e5565467b29309f351c0edb

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    1
+  elsif  a > b
+    -1
+  end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+array[1], array[2] = array[2], array[1]
+array
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |strng|
+    x = strng.split("")
+    x[2] = "$"
+    new_array << x.join
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select{|strng| strng.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, i| sum + i}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index == 1
+       word
+    else
+       "#{word}s"
+    end
+    end
+end
